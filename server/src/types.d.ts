79a3593417aa2df73fbee36ea8b5447edd00bf0d
IDRAYNAR <rayan.idri@edu.devinci.fr>
2025-03-04 17:47:25 +0100

    fix(deps): Update Express dep

--- server/src/types.d.ts
+++ server/src/types.d.ts
@@ -1,7 +1,15 @@
 // Déclarations de types pour les modules qui posent problème
 declare module 'express' {
-  import * as e from 'express-serve-static-core';
-  export = e;
+  import express = require('express-serve-static-core');
+  
+  // Ajouter les fonctions et propriétés manquantes
+  const e: express.Express & {
+    json: () => express.RequestHandler;
+    urlencoded: (options?: { extended?: boolean }) => express.RequestHandler;
+    static: (root: string, options?: any) => express.RequestHandler;
+  };
+  
+  export default e;
 }
 
 declare module 'cors' {
